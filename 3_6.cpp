bda0b60996821dbf4254a2218e1a62aac620c885
ThomasCaneday <thomascaneday@gmail.com>
2025-03-31 17:02:41 -0700

    "Fix: 3_6 assignment finished"

--- 3_6.cpp
+++ 3_6.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 
-void f() {
-    
-}
-
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    const float factor = 2.54; // 1 inch equals 2.54 cm
+    float x, in, cm;
+    char ch = 0;
+
+    std::cout << "enter length then type: ";
+
+    std::cin >> x; // read a floating-point number
+    std::cin >> ch; // read a suffix
+
+    switch(ch) {
+    case 'i': // inch
+        in = x;
+        cm = x*factor;
+        break;
+    case 'c': // cm
+        in = x/factor;
+        cm = x;
+        break;
+    default:
+        in = cm = 0;
+        break;
+    }
+
+    std::cout << in << " in = " << cm << " cm\n";
 }
