d6c6b6235476b2b83b1a5d60401e4bdb48bca389
ThomasCaneday <thomascaneday@gmail.com>
2025-04-14 15:36:28 -0700

    Add: finished 3.8.4 exercise

---
+++ 3_8_4.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+std::map<std::string, int> histogram;
+
+void record(const std::string& s) {
+    histogram[s]++;
+}
+
+void print(const std::pair<const std::string, int>& r) {
+    std::cout << r.first << ' ' << r.second << '\n';
+}
+
+bool gt_42(const std::pair<const std::string, int>& r) {
+    return r.second>42;
+}
+
+void f(std::map<std::string, int>& m) {
+    typedef std::map<std::string, int>::const_iterator MI;
+    MI i = find_if(m.begin, m.end(), gt_42);
+}
+
+void g(const std::map<std::string, int>& m) {
+    int c42 = count_if(m.begin(), m.end(), gt_42);
+}
+
+int main() {
+    std::istream_iterator<std::string> ii(std::cin);
+    std::istream_iterator<std::string> eos;
+
+    for_each(ii, eos, record);
+    for_each(histogram.begin(), histogram.end(), print);
+
+    return 0;
+}
